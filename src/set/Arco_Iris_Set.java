c96573d91cf973c96e476f955df5aaf23f30fe78
Jamela <jonathanlinch7@gmail.com>
2022-05-24 17:56:41 -0300

    final set

--- src/set/Arco_Iris_Set.java
+++ src/set/Arco_Iris_Set.java
@@ -2,6 +2,7 @@ package set;
 
 import java.util.*;
 
+
 /*
  vermelha, laranja, amarela, verde, azul, azul-escuro e violeta*/
 
@@ -32,6 +33,27 @@ public class Arco_Iris_Set{
         }
 
         System.out.println("\t---Ordem inversa---\t");
+        Set<String> coresInversas = new LinkedHashSet<>(
+                Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+        System.out.println(coresInversas);
+        List<String> listCores = new ArrayList<>(coresInversas);
+        Collections.reverse(listCores);
+        System.out.println(listCores);
 
+        System.out.println("\"Exiba todas as cores que começam com a letra ”v”: ");
+        for (String cor:arco_iris) {
+            if (cor.startsWith("v")) System.out.println(cor);
+        }
+
+        System.out.println("\"Remova todas as cores que não começam com a letra ”v”: ");
+        Iterator<String> iterator = arco_iris.iterator();
+        while(iterator.hasNext()){
+            if (!iterator.next().startsWith("v")) iterator.remove();
+        }
+        System.out.println(arco_iris);
+
+        System.out.println("Limpe o conjunto");
+        arco_iris.clear();
+        System.out.println(arco_iris);
     }
 }
